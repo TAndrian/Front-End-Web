5f5e493097a7f3099e68b06c58689efc31c36c63
Le Roy Francois <francois.leroy@etudiant.univ-rennes1.fr>
2021-02-15 15:47:19 +0100

    ajout du routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {FormulaireNouveauChantierComponent} from './formulaires/formulaire-nouveau-chantier/formulaire-nouveau-chantier.component';
+import {FormulaireClientComponent} from './formulaires/formulaire-client/formulaire-client.component';
+import {FormulaireSiteComponent} from './formulaires/formulaire-site/formulaire-site.component';
+import {FormulaireDemandeChantierComponent} from './formulaires/formulaire-demande-chantier/formulaire-demande-chantier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: FormulaireDemandeChantierComponent},
+    {path: 'demandeDeChantier', component: FormulaireDemandeChantierComponent},
+    {path: 'nouveauChantier', component: FormulaireNouveauChantierComponent},
+    {path: 'client', component: FormulaireClientComponent},
+    {path: 'site', component: FormulaireSiteComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
