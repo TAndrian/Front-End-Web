808008473e5ed51496b4a6f7224825dfbd37188f
17010890 <chi-linh.pham@etudiant.univ-rennes1.fr>
2021-02-25 10:17:14 +0100

    update button navigation

--- src/app/formulaires/formulaire-administration/formulaire-administration.component.ts
+++ src/app/formulaires/formulaire-administration/formulaire-administration.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Chantier} from '../../shared/model/chantier';
 import {ChantierGet} from '../../shared/model/chantierGet';
 import {FormControl, FormGroup} from '@angular/forms';
+import {ChantierService} from 'src/app/core/services/chantier.service';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-formulaire-administration',
@@ -12,7 +14,7 @@ export class FormulaireAdministrationComponent implements OnInit {
   @Input() chantier: ChantierGet;
   formAdministration: FormGroup;
   numbers: number[] = [];
-  constructor() {
+  constructor(private chantierService: ChantierService) {
   }
 
   ngOnInit(): void {
@@ -28,5 +30,31 @@ export class FormulaireAdministrationComponent implements OnInit {
           this.formAdministration.addControl(String(i), new FormControl(''));
       }
   }
+
+  onSubmit(): void {
+        /*const materiel = this.formAdministration.materiel.value;
+        const conducteur = this.formAdministration.conducteur.value;
+        const dateDebutTheorique = this.formAdministration.dateDebutTheorique.value;
+        const dateFinTheorique = this.formAdministration.dateFinTheorique.value;
+        let chantierUpdated: Chantier;
+        if (this.regularite) {
+            chantierUpdated = new Chantier(
+                this.chantier.siteId, this.chantier.clientId, null, null, null, null, null , materiel, dateDebutTheorique, dateFinTheorique,
+                null, null, null, null, null, null, null, null,
+                conducteur, true, this.chantier.statusIntervention, null, null, null,  null, null, null);
+        } else {
+            chantierUpdated = new Chantier(
+                site.id, client.id, null, null, adresse, null, nbOuvrier, null, null, null,
+                estimationTemps, telephone, statusChantier, nomChantier, informationsInterne, description, null, null,
+                true, regularite,  this.chantier.statusIntervention, null, null, null);
+        }
+        this.chantierService.updateChantierById(this.chantier.id + '', chantierUpdated).subscribe(
+            (res: HttpResponse<any>) => {
+                console.log(res.headers.get('Location'));
+                this.ngOnInit();
+                window.location.reload();
+            }
+        );*/
+    }
 
 }
